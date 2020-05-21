e76626da7681ae34b258b15537be15881ce9c64c
Javier Calvarro Nelson <jacalvar@microsoft.com>
2020-05-21 02:34:21 -0700

    Add project files.

---
+++ GoogleAuth/Program.cs
@@ -0,0 +1,35 @@
+using System;
+using System.Net.Http;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using System.Text;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+
+namespace GoogleAuth
+{
+    public class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            var builder = WebAssemblyHostBuilder.CreateDefault(args);
+            builder.RootComponents.Add<App>("app");
+
+            builder.Services.AddTransient(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+
+            builder.Services.AddOidcAuthentication(options =>
+            {
+                options.ProviderOptions.Authority = "https://accounts.google.com/";
+                options.ProviderOptions.RedirectUri = "https://localhost:5001/authentication/login-callback";
+                options.ProviderOptions.PostLogoutRedirectUri = "https://localhost:5001/authentication/logout-callback";
+                options.ProviderOptions.ClientId = "1048855053233-16qpvegahk2leksb6q2hheoas56pfib9.apps.googleusercontent.com";
+                options.ProviderOptions.ResponseType = "id_token";
+            });
+
+            await builder.Build().RunAsync();
+        }
+    }
+}
